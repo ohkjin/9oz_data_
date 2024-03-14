f2c0060628b6a2e49f67465763f7d496928c7b3d
pnusw14 <dustofjade@gmail.com>
2024-03-14 18:09:38 +0900

    postman

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ import torch.nn as nn
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -26,6 +25,44 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+@app.route('/test', methods=['POST'])
+def segment():
+    request_obj = request.files['file']
+    print('request_obj',request_obj)
+    image_bytes = io.BytesIO(request_obj.read())
+    img = Image.open(image_bytes)
+    img_base64 = base64.b64encode(image_bytes.read())
+    print(type(img))
+    files = {'file': open(img, 'rb')}
+    # response = requests.post('http://springboot_server:port/receive_image', files=files)
+   
+
+    
+     # image segmentation
+    # processor = SegformerImageProcessor.from_pretrained("mattmdjaga/segformer_b2_clothes")
+    # model = AutoModelForSemanticSegmentation.from_pretrained("mattmdjaga/segformer_b2_clothes")
+    
+    # inputs = processor(images=img, return_tensors="pt")
+
+    # outputs = model(**inputs)
+    # logits = outputs.logits.cpu()
+
+    # upsampled_logits = nn.functional.interpolate(
+    #     logits,
+    #     size=img.size[::-1],
+    #     mode="bilinear",
+    #     align_corners=False,
+    # )
+
+    # pred_seg = upsampled_logits.argmax(dim=1)[0]
+    # plt.imshow(pred_seg)
+    # print(upsampled_logits[0][4].detach().numpy())
+    
+    # return jsonify({'file':str(img_base64)})
+    return send_file(image_file, mimetype='image/jpeg')
+    # 이미지 열기
+
 @app.route('/photo_to_flask', methods=['POST'])
 def getPhotoInput():
     # BE에서 json객체 전달받기
@@ -34,9 +71,9 @@ def getPhotoInput():
     response_obj = {
         # 'categories':'',
         # 'nukki_image':'',
-        'similar':'',
-        'style':0,
-        'season':'봄',
+        # 'similar':'',
+        # 'style':0,
+        # 'season':'봄',
         'success':'',
         'message':'',
     }
@@ -52,7 +89,6 @@ def getPhotoInput():
         # rawBytes.seek(0)
         # img_base64 = base64.b64encode(rawBytes.read())
         # return jsonify({'status':str(img_base64)})
-
         # 이미지 열기
         byte_file = request_obj['image'] ## byte file
         # base64
@@ -105,4 +141,5 @@ def getPhotoInput():
     #     filename = secure_filename(file.filename)
     #     file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
     #     return redirect(url_for('download_file',name=filename))
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
